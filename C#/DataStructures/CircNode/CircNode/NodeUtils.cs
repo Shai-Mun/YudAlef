5f8ae863dcf422934ddd44b35ce3c6046489b9a4
Shai-Mun <shaimuntner@gmail.com>
2025-11-06 23:04:12 +0200

    Add files via upload

---
+++ C#/DataStructures/CircNode/CircNode/NodeUtils.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircNode
+{
+    class NodeUtils
+    {
+
+        // {5,2,9,-2}
+        public static Node<T> BuildChainFromArray<T>(T[] a)
+        {
+            Node<T> head = new Node<T>(a[0]);
+            Node<T> p = head;
+
+            for (int i = 1; i < a.Length; i++)
+            {
+                Node<T> n1 = new Node<T>(a[i]);
+                p.SetNext(n1);
+                p = n1; // p = p.GetNext();
+            }
+            return head;
+        }
+
+        public static Node<T> BuildChainFromArray2<T>(T[] a)
+        {
+            Node<T> head = null;
+            for (int i = a.Length -1 ; i >= 0; i--)
+                 head = new Node<T>(a[i], head);
+            return head;
+        }
+
+
+        public static int Sum (Node<int> lst)
+        {
+            Console.WriteLine("1----");
+            Console.WriteLine(lst);
+            int sum = 0;
+            while(lst != null)
+            {
+                sum += lst.GetValue();
+                lst = lst.GetNext();
+            }
+            
+            Console.WriteLine(lst);
+            Console.WriteLine("2----");
+            return sum;
+        }
+
+        public static Node<int> DeleteFirst(Node<int> lst)
+        {
+            Console.WriteLine("3----");
+            Console.WriteLine(lst);
+            lst = lst.GetNext();
+            
+            Console.WriteLine("4----");
+            Console.WriteLine(lst);
+
+            return lst;
+        }
+
+
+    }
+}
---
+++ C#/DataStructures/CircNode/CircNode/Program.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircNode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 };
+            Node<int> lst = NodeUtils.BuildChainFromArray(arr);
+
+
+            MakeCircular(lst, 6);
+            Console.WriteLine(IsCircular(lst));
+            if (CirclePoint(lst) != null)
+                Console.WriteLine(CirclePoint(lst).GetValue());
+            Console.WriteLine(Length(lst));
+            PrintNode(lst);
+
+            DisconnectCircle(lst);
+
+            Console.WriteLine(IsCircular(lst));
+            if (CirclePoint(lst) != null)
+                Console.WriteLine(CirclePoint(lst).GetValue());
+            Console.WriteLine(Length(lst));
+            PrintNode(lst);
+        }
+
+        public static void MakeCircular<T>(Node<T> lst, int n)
+        {
+            Node<T> p = lst;
+            int cnt = 0;
+            Node<T> connection = null;
+            while (p.GetNext() != null)
+            {
+                if (cnt == n)
+                    connection = p;
+                cnt++;
+                p = p.GetNext();
+            }
+            p.SetNext(connection);
+
+        }
+
+        public static bool IsCircular<T>(Node<T> lst)
+        {
+            Node<T> pSlow = lst;
+            Node<T> pFast = lst.GetNext();
+
+            if (pFast == null)
+                return false;
+
+            while (pFast.GetNext() != null && pFast.GetNext().GetNext() != null)
+            {
+                if (pSlow == pFast)
+                    return true;
+                pSlow = pSlow.GetNext();
+                pFast = pFast.GetNext().GetNext();
+            }
+
+            return false;
+        }
+
+        public static Node<T> CirclePoint<T>(Node<T> lst)
+        {
+            if (!IsCircular(lst))
+                return null;
+
+            Node<T> pSlow = lst;
+            Node<T> pFast = lst;
+
+            bool first = true;
+            while (pSlow != pFast || first)
+            {
+                pSlow = pSlow.GetNext();
+                pFast = pFast.GetNext().GetNext();
+                first = false;
+            }
+
+            pSlow = lst;
+            while (pSlow != pFast)
+            {
+                pSlow = pSlow.GetNext();
+                pFast = pFast.GetNext();
+            }
+
+            return pSlow;
+        }
+
+        public static int Length<T>(Node<T> lst)
+        {
+            Node<T> tmp = lst;
+            int cnt = 0;
+
+            if (!IsCircular(lst))
+            {
+                while (tmp != null)
+                {
+                    cnt++;
+                    tmp = tmp.GetNext();
+                }
+            }
+            else
+            {
+                Node<T> Circle = CirclePoint(lst);
+                while (tmp != Circle)
+                {
+                    cnt++;
+                    tmp = tmp.GetNext();
+                }
+                cnt++;
+                tmp = tmp.GetNext();
+                while (tmp != Circle)
+                {
+                    cnt++;
+                    tmp = tmp.GetNext();
+                }
+            }
+            return cnt;
+        }
+
+        public static void PrintNode<T>(Node<T> lst)
+        {
+            Node<T> tmp = lst;
+
+            if (!IsCircular(lst))
+            {
+                while (tmp.GetNext().GetNext() != null)
+                {
+                    Console.Write(tmp.GetValue() + "->");
+                    tmp = tmp.GetNext();
+                }
+                Console.Write(tmp.GetNext().GetValue()+"\n");
+            }
+            else
+            {
+                Node<T> Circle = CirclePoint(lst);
+                
+                while (tmp != Circle)
+                {
+                    Console.Write(tmp.GetValue() + "->");
+                    tmp = tmp.GetNext();
+                }
+                Console.Write("(");
+                while (tmp.GetNext() != Circle)
+                {
+                    Console.Write(tmp.GetValue() + "->");
+                    tmp = tmp.GetNext();
+                }
+                Console.Write(tmp.GetValue() + ")\n");
+            }
+        }
+
+        public static void DisconnectCircle<T>(Node<T> lst)
+        {
+            if (!IsCircular(lst))
+                return;
+
+            Node<T> Circle = CirclePoint(lst);
+            Node<T> tmp = Circle;
+
+            while (tmp.GetNext() != Circle)
+            {
+                tmp = tmp.GetNext();
+            }
+            tmp.SetNext(null);
+
+        }
+    }
+}

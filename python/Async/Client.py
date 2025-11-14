1c26afe9840914f51ae3870ff5dfe92852d1ea4f
Shai-Mun <shaimuntner@gmail.com>
2025-11-14 11:28:07 +0200

    Async

--- python/Async/Client.py
+++ python/Async/Client.py
@@ -1,82 +1,118 @@
+__author__ = 'Yossi'
+
+from sys import argv
+
 import socket
-import sys
+import time
 
-def handle_reply(reply):
-    handle_info = ""
-    command = reply[:4]
+import threading
 
-    if command == "HOUR":
-        handle_info = handle_hour(reply)
-    elif command == "ERR_":
-        handle_info = handle_err_(reply)
-    elif command == "MESG":
-        handle_info = handle_mesg(reply)
-    return handle_info+"\n"
+from tcp_by_size import send_with_size, recv_by_size
+
+input_data = ""
+close_thread = ""
+input_lock = threading.Lock()
 
 
-def handle_mesg(reply):
-    s = reply.split("~")
-    cli_message = "The server sent: " + s[1]
-    return cli_message
+class Input_thread(threading.Thread):
+    """
+    use global to indicate new command from user
+    """
+
+    def __init__(self):
+        threading.Thread.__init__(self)
+
+    def run(self):
+        global input_data
+        time.sleep(2)
+        while not close_thread:
+            input_lock.acquire()
+            input_data = input("Enter_data to send>")
+            input_lock.release()
+            time.sleep(0.2)  # prevent busy waiting
 
 
-def handle_err_(reply):
-    s = reply.split("~")
-    cli_error = ""
+def main(ip, user_name):
+    global input_data, close_thread
 
-    error = s[1]
-    match int(error):
-        case 1:
-            cli_error = "Command doesn't exist."
-        case 2:
-            cli_error = "Invalid input."
-        case 3:
-            cli_error = "Place doesn't exist."
-        case 4:
-            cli_error = "I don't know the time."
-        case 5:
-            cli_error = "Connection ended unexpectedly."
-        case 6:
-            cli_error = "There is a problem with the geolocator."
-    return cli_error
+    cli_s = socket.socket()
+    if not ip or len(ip) < 7:
+        ip = "HertzogSchool.pythonanywhere.com"
+    cli_s.connect((ip, 5050))
 
+    cli_s.settimeout(0.3)
+    print("For Private type P:name:<message>\n")
 
-def handle_hour(reply):
-    s = reply.split("~")
-    cli_time = f"Your time is {s[1]}:{s[2]}:{s[3]}\n"
-    return cli_time
+    close_thread = False
+    input_t = Input_thread()
+    input_t.start()
 
+    while True:
+        data = ""
+        if input_data == "q":
+            break
+        if input_data != "":
+            data = input_data
+            input_lock.acquire()
+            input_data = ""
+            input_lock.release()
+            if data[:2] == "P:":
+                fields = data.split(":")
+                msg = "PRV|" + user_name + "|" + fields[1] + "|" + fields[2]
+                send_with_size(cli_s, bytearray(msg, 'utf8'))
+            else:
+                msg = "PUB|" + user_name + "|" + data
+                send_with_size(cli_s, bytearray(msg, 'utf8'))
 
-sock = socket.socket()
+        try:
 
-ip = '127.0.0.1'
-port = 3001
+            byte_data = recv_by_size(cli_s)
+            data = byte_data.decode()
+            if data == "":
+                print("seems server DC")
+                break
+            print("Got data >>> " + data)
+            fields = data.split("|")
+            msg_type = data[:3]
+            if msg_type == "NAM":
+                msg = "NMR|" + user_name + ":" + user_name[::-1]
+                send_with_size(cli_s, bytearray(msg, 'utf8'))
+            elif msg_type == "MSG":
+                print(fields[1] + ": " + fields[2])
 
-sock.connect((ip, port))
+        except socket.error as err:
 
-print("Connected to server\n\n")
-option = input("Options:\nEnter 1 to enter city name\nEnter exit to exit\n>")
-data = ""
+            if err.errno == 10035 or str(err) == "timed out":  # if we use conn.settimeout(x)
+                continue
+            if err.errno == 10054:
+                # 'Connection reset by peer'
+                print("Error %d Client is Gone. %s reset by peer." % (err.errno, str(cli_s)))
+                break
+            else:
+                print("%d General Sock Error Client %s disconnected" % (err.errno, str(cli_s)))
+                break
 
-while option != "exit":
-    try:
-        if option == "1":
-            data = "GTIM~" + input("Enter city name, first letter capital\n>")
-        sock.send(data.encode())
-
-        sock.settimeout(5.0)
-        data = sock.recv(100).decode()
-        if data == "":
-            print("Server Disconnected")
+        except Exception as err:
+            print("General Error:", err.message)
             break
 
-        info = handle_reply(data)
-        print(info)
+    close_thread = True
+    print("Press Enter for exit")
+    cli_s.close()
+    input_t.join()
 
-        option = input("Options:\nEnter 1 to enter city name\nEnter exit to exit\n>")
+    print("Bye Bye from " + user_name)
 
-    except socket.timeout:
-        print("Socket operation timed out!")
 
-print("Disconnected from server")
-sock.close()
+if __name__ == "__main__":
+    if len(argv) < 3:
+        addr = "HertzogSchool.pythonanywhere.com"
+        u_name = "avi"
+        main(addr, u_name)
+
+        # print( "you must enter <IP> <username>")
+        # exit()
+    else:
+        addr = argv[1]
+        u_name = argv[2]
+        main(addr, u_name)
--- python/Async/Server.py
+++ python/Async/Server.py
@@ -1,87 +1,155 @@
+__author__ = 'Yossi'
 import socket
+import time
+import threading
 
-from geopy.geocoders import ArcGIS
-from datetime import datetime
-import requests
+from  tcp_by_size import send_with_size ,recv_by_size
+from AsyncMessages import AsyncMessages
 
+async_msg = None
 
-def handle_message(request):
-    handle_reply = ""
-    command = request[:4]
-
-    if command == "GTIM":
-        handle_reply = handle_gtim(request)
-    else:
-        handle_reply = "ERR_~001"
-
-    return handle_reply
+EWOULDBLOCK = 10035
 
 
-def handle_gtim(request):
-    s = request.split("~")
+def handle_message(data, user_name):
+    global async_msg
 
-    geolocator = ArcGIS()
+    to_send = ""
+    fields = data.split("|")
+    msg_type = fields[0]
 
-    if not s[1].replace(" ", "").isalpha():
-        return "ERR_~002"
+    if msg_type == "PUB":
+        from_user = fields[1]
+        public_msg = fields[2]
+        for user in async_msg.sock_by_user.keys():
+            if user != user_name:
+                async_msg.put_msg_by_user("MSG|" + from_user + "|" + public_msg,user)
+    if msg_type == "PRV":
+        from_user = fields[1]
+        to_user = fields[2]
+        msg = fields[3]
+        async_msg.put_msg_by_user("MSG|" + from_user + "|" + msg,to_user)
 
-    try:
-        location = geolocator.geocode(s[1])
-    except:
-        return "ERR_~006"
-    if not location:
-        return "ERR_~003"
+    return to_send
 
-    lat, lon = location.latitude, location.longitude
-
-    url = f"https://timeapi.io/api/Time/current/coordinate?latitude={lat}&longitude={lon}"
-    response = requests.get(url)
-    if response.status_code != 200:
-        return "ERR_~004"
-
-    time_data = response.json()
-    time_str = time_data.get("dateTime")
-
-    dt = datetime.fromisoformat(time_str)
-    return f"HOUR~{dt.hour}~{dt.minute}~{dt.second}"
+    def check_user_pass(u,p):
+        """
+        check in some db user and password
+        :param u:
+        :param p:
+        :return:
+        """
+        print ("name:" + u + " pass:" + p + " checked")
+        return True
 
 
-srv_sock = socket.socket()
-ip = '127.0.0.1'  # '192.168.68.115'
-port = 3001
-srv_sock.bind((ip, port))
+    def handl_client(sock , tid):
+        """
+        main thread - recv handle and answer also push async msgs
+        :param sock: socket
+        :param tid: thread is
+        :return:
+        """
+        global async_msg
 
-srv_sock.listen(5)
 
-while True:
-    print("Waiting for a connection...")
+    user_name = ""
+    exit_thread = False
 
-    cli_sock, address = srv_sock.accept()
-    print("Client connected\n\n")
-    while True:
-        reply = ""
+    print( "New Client num " + str(tid))
+    to_send = "NAM|<uname>:<pass>"
+
+    got_name = False;
+    while not got_name:
+        send_with_size(sock, bytearray(to_send,'utf8'))
+        byte_data = recv_by_size(sock)
+        data = byte_data.decode()
+        if data == "":
+            print( "Client disconnected")
+            exit_thread = True
+            break
+        if data[:3] == "NMR" and len(data) > 6 :
+            got_name = True
+            fields = data[4:].split(':')
+            user_name =  fields[0]
+            password = fields[1]
+            if check_user_pass(user_name,password):
+                async_msg.sock_by_user[user_name] = sock
+            else:
+                exit_thread = True
+    sock.settimeout(0.3)
+
+    while not exit_thread :
         try:
-            cli_sock.settimeout(5.0)
-            data = cli_sock.recv(100).decode()
 
+            byte_data = recv_by_size(sock)
+            data = byte_data.decode()
             if data == "":
-                print("Client Disconnected")
+                print( "Error: Seems Client DC")
                 break
 
-            reply = handle_message(data)
+            to_send = handle_message(data,user_name)
+            if to_send != "":
+                send_with_size(sock, bytearray(to_send,'utf8'))
 
-        except socket.timeout:
-            print("Socket operation timed out!")
+        except socket.error as  err:
 
-        option = input("Options:\nEnter 1 to send the client a message\nEnter exit to exit\n>")
-        if option == "1":
-            reply = "MESG~" + input("Enter a message:\n>")
+            if err.errno == EWOULDBLOCK or str(err) == "timed out":  # if we use conn.settimeout(x)
+                msgs = async_msg.get_async_messages_to_send(sock)
+                for data in msgs:
+                    send_with_size(sock, bytearray(data,'utf8'))
+                    time.sleep(0.1)
+                continue
 
-        if reply != "":
-            cli_sock.send(reply.encode())
-        else:
-            cli_sock.send("ERR_~005".encode())
+            if err.errno == 10054:
+                #'Connection reset by peer'
+                print( "Error %d Client is Gone.  reset by peer." % (err.errno))
+                break
+            else:
+                print( "%d General Sock Error Client disconnected" % (err.errno))
+                break
+
+        except Exception as err:
+            print( "General Error:", str(err))
+            break
+    async_msg.delete_socket(sock)
+    sock.close()
 
 
 
-    cli_sock.close()
+    def main ():
+    global async_msg
+
+    s = socket.socket()
+
+    async_msg = AsyncMessages()
+
+    s.bind(("0.0.0.0", 5050))
+
+    s.listen(4)
+    print( "after listen")
+
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+
+    threads = []
+    i = 1
+    while True:
+        cli_s , addr = s.accept()
+        print ("New Client")
+        async_msg.add_new_socket(cli_s)
+
+        t = threading.Thread(target = handl_client, args=(cli_s, i))
+        t.start()
+        i+=1
+        threads.append(t)
+
+    for t in threads:
+        t.join()
+    s.close()
+    print( "Bye ..")
+
+
+
+    if __name__ == "__main__":
+    main()

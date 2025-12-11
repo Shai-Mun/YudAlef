74ce7f14e94050905f223dfc07e4ef818889254e
Shai-Mun <shaimuntner@gmail.com>
2025-12-11 11:46:02 +0200

    wip

--- python/4_4/server.py
+++ python/4_4/server.py
@@ -7,6 +7,10 @@ NOT_FOUND_RESPONSE = ('HTTP/1.1 404 Not Found\r\nContent-Length: 27\r\nContent-T
                   '<h1>404 File Not Found</h1>')
 ERROR_RESPONSE = ('HTTP/1.1 500 Internal Server Error\r\nContent-Length: 34\r\nContent-Type: text/html; charset=utf-8\r\n\r\n' +
                   '<h1>500 Internal Server Error</h1>')
+PERMISSION_RESPONSE = ('HTTP/1.1 403 Forbidden\r\nContent-Length: 22\r\nContent-Type: text/html; charset=utf-8\r\n\r\n' +
+                  '<h1>403 Forbidden</h1>')
+VALID_PERM = ['/css', '/imgs', '/js', '/index.html']
+
 
 def main():
     # SERVER SIDE:
@@ -37,29 +41,48 @@ def main():
                 resource = '/index.html'
             filename = '.' + resource
 
+            params = resource[resource.find("?")+1:]
             if '/calculate-next' in resource:
-                num = int(resource.split("=")[1])
-                html = f'<h1>{num}</h1>'
-                cli.send(f'HTTP/1.1 200 OK\r\nContent-Length: {len(str(html))}]\r\nContent-Type: text/html; charset=utf-8\r\n\r\n'.encode() + html.encode())
+                num = int(params.split("=")[1])
+                html = f'<h1>{num+1}</h1>'
+                cli.send(f'HTTP/1.1 200 OK\r\nContent-Length: {len(str(html))}\r\nContent-Type: text/html; charset=utf-8\r\n\r\n'.encode() + html.encode())
+
+            elif '/calculate-area' in resource:
+                num1, num2 = (float(x[str(x).find("=")+1:]) for x in params.split("&"))
+                html = f'<h1>{(num1*num2)/2}</h1>'
+                cli.send(f'HTTP/1.1 200 OK\r\nContent-Length: {len(str(html))}\r\nContent-Type: text/html; charset=utf-8\r\n\r\n'.encode() + html.encode())
+
+            elif '/upload' in resource:
+                upload_name = params.split("=")[1]
+                with open(f'./uploads/{upload_name}', 'wb') as f:
+                     f.write(body)
 
             elif os.path.isfile(filename):
-                with open(filename, 'rb') as f:
-                    data = f.read()
-                file_type = filename.split(".")[filename.count(".")]
-                resp_headers = ''
-                match file_type:
-                    case "txt" | "html":
-                        resp_headers = 'Content-Type: text/html; charset=utf-8'
-                    case "jpg":
-                        resp_headers = 'Content-Type: image/jpeg'
-                    case "js":
-                        resp_headers = 'Content-Type: text/javascript; charset=UTF-8'
-                    case "css":
-                        resp_headers = 'Content-Type: text/css'
-                    case "ico":
-                        resp_headers = 'Content-Type: image/x-icon'
-                resp_headers += '\r\n'
-                http_send(cli, OK_RESPONSE, resp_headers, data)
+                if resource[:resource[1:].find("/")+1] in VALID_PERM or resource in VALID_PERM:
+
+                    with open(filename, 'rb') as f:
+                        data = f.read()
+                    file_type = filename.split(".")[filename.count(".")]
+                    resp_headers = ''
+                    match file_type:
+                        case "txt" | "html":
+                            resp_headers = 'Content-Type: text/html; charset=utf-8'
+                        case "jpg":
+                            resp_headers = 'Content-Type: image/jpeg'
+                        case "js":
+                            resp_headers = 'Content-Type: text/javascript; charset=UTF-8'
+                        case "css":
+                            resp_headers = 'Content-Type: text/css'
+                        case "ico":
+                            resp_headers = 'Content-Type: image/x-icon'
+                        case "gif":
+                            resp_headers = 'Content-Type: image/gif'
+                    resp_headers += '\r\n'
+                    http_send(cli, OK_RESPONSE, resp_headers, data)
+
+                else:
+                    cli.send(PERMISSION_RESPONSE.encode())
+
             else:
                 cli.send(NOT_FOUND_RESPONSE.encode())
 
